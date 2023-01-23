b729fead0a9c122e2367cd479cc77b8b8678f124
Isaac <isaac.lin.pnc@gmail.com>
2023-01-23 12:25:29 -0500

    worked on lab

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const searchForm = document.querySelector("#github-form")
+console.log(searchForm)
+searchForm.addEventListener("submit", getUser)
+
+function handleSubmit(e) {
+  e.preventDefault()
+  let userObj = {
+    login: e.target.name.value,
+    image: e.target.image.value,
+    likes: 0
+  }
+  renderOneToy(toyObj)
+  postToy(toyObj)
+}
+
+function renderUser(user) {
+    let card = document.createElement("div")
+    card.className = "card"
+    card.innerHTML = `
+    <h2>${user.login}</h2>
+    <img src="${user.avatar_url}/>
+    <p>${user.html_url}</p>
+    `
+}
+
+document.querySelector("#github-container").appendChild(card)
+
+const search = document.querySelector("#search")
+function getUser(e) {
+    e.preventDefault()
+    console.log(search.value)
+    // fetch (`https://api.github.com/search/users?q=${search.value}`)
+    // .then(resp => resp.json())
+    // .then(user => renderUser(user))
+}
